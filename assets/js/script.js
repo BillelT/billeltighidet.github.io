2af119c45697c28ff79950b2fd9a96833749cd51
LeWilSy <b.tighidet0@gmail.com>
2023-12-28 23:55:43 +0100

    COMPLETE RESPONSIVE, MOBILE MENU? SOME DETAILS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,37 @@
-const dateActuelle = new Date();
+// Declare global variables used in this script
 const dateSpan = document.getElementById("currentDate");
 const hourSpan1 = document.getElementById("currentHour1");
 const hourSpan2 = document.getElementById("currentHour2");
-const hour = {
-  hour: "numeric",
-  minute: "numeric",
-};
-const dateFormate = dateActuelle.toLocaleDateString("fr-FR", hour);
+const navLinks = ["Projets", "Contact", "Mon CV"];
 
-dateSpan.innerText = dateFormate.slice(0, 10);
-hourSpan1.innerText = dateFormate.slice(11, 13);
-hourSpan2.innerText = dateFormate.slice(14, 16);
+function dateAndhour() {
+  let date = new Date(); // create an object date
+  let hour = {
+    // define hour format
+    hour: "numeric",
+    minute: "numeric",
+  };
+  let dateFormate = date.toLocaleDateString("fr-FR", hour); // stocking date and hour in a variable string
 
-console.log(dateFormate.slice(11, 13), dateFormate.slice(14, 16));
+  dateSpan.innerText = dateFormate.slice(0, 10);
+  hourSpan1.innerText = dateFormate.slice(11, 13);
+  hourSpan2.innerText = dateFormate.slice(14, 16);
+}
+
+dateAndhour();
+setInterval(dateAndhour, 1000);
+
+// mobile menu class adding and CTA's innerText adaptation
+document.addEventListener("click", (e) => {
+  if (e.target.innerText === "Menu") {
+    mobileNavClicked.classList.toggle("mobile-nav-clicked");
+    mobileNav.innerText = "X";
+  } else if (e.target.innerText === "X") {
+    mobileNavClicked.classList.toggle("mobile-nav-clicked");
+    mobileNav.innerText = "Menu";
+  }
+  if (navLinks.includes(e.target.innerText)) {
+    mobileNavClicked.classList.toggle("mobile-nav-clicked");
+    mobileNav.innerText = "Menu";
+  }
+});
