ca2803637a6a482ff1aedb9b073f4fa084f8d507
LeWilSy <b.tighidet0@gmail.com>
2023-12-29 12:15:41 +0100

    SECTION PROJECTS TEXT SIZE AND ALIGNEMENT CORRECT°

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,6 @@ const hourSpan1 = document.getElementById("currentHour1");
 const hourSpan2 = document.getElementById("currentHour2");
 const navLinks = ["Projets", "Contact", "Mon CV"];
 
-function dateAndhour() {
-  let date = new Date(); // create an object date
-  let hour = {
-    // define hour format
-    hour: "numeric",
-    minute: "numeric",
-  };
-  let dateFormate = date.toLocaleDateString("fr-FR", hour); // stocking date and hour in a variable string
-
-  dateSpan.innerText = dateFormate.slice(0, 10);
-  hourSpan1.innerText = dateFormate.slice(11, 13);
-  hourSpan2.innerText = dateFormate.slice(14, 16);
-}
-
-dateAndhour();
-setInterval(dateAndhour, 1000);
-
 // mobile menu class adding and CTA's innerText adaptation
 document.addEventListener("click", (e) => {
   if (e.target.innerText === "Menu") {
