95bf28165f972c7d6139f9858e995d82ceb1440b
LeWilSy <b.tighidet0@gmail.com>
2024-01-09 10:01:18 +0100

    LINK PROJECT CHANGED + MOBILE MENU UDL + FLEXMODIF

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
 // Declare global variables used in this script
-const dateSpan = document.getElementById("currentDate");
-const hourSpan1 = document.getElementById("currentHour1");
-const hourSpan2 = document.getElementById("currentHour2");
 const navLinks = ["Projets", "Contact", "Mon CV"];
 
 // mobile menu class adding and CTA's innerText adaptation
