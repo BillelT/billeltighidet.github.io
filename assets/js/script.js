ef4da3665de85c8d5f0d62d53ab1e6f09c87e9dd
LeWilSy <b.tighidet0@gmail.com>
2024-01-14 12:57:57 +0100

    CV FILE UPDATE + ANIM BURGER OPTIMIZATION

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,19 @@ const year = Dates.getFullYear();
 document.addEventListener("click", (e) => {
   if (e.target.innerText === "Menu") {
     mobileNavClicked.classList.toggle("mobile-nav-clicked");
+    test2.classList.toggle("mobile-nav-clicked");
+    test3.classList.toggle("mobile-nav-clicked");
     mobileNav.innerText = "X";
   } else if (e.target.innerText === "X") {
     mobileNavClicked.classList.toggle("mobile-nav-clicked");
+    test2.classList.toggle("mobile-nav-clicked");
+    test3.classList.toggle("mobile-nav-clicked");
     mobileNav.innerText = "Menu";
   }
   if (navLinks.includes(e.target.innerText)) {
     mobileNavClicked.classList.toggle("mobile-nav-clicked");
+    test2.classList.toggle("mobile-nav-clicked");
+    test3.classList.toggle("mobile-nav-clicked");
     mobileNav.innerText = "Menu";
   }
 })
