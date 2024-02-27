dd37036600b052f4f4d649d0f6a7800fba451abb
LeWilSy <b.tighidet0@gmail.com>
2024-02-27 14:46:00 +0100

    dark theme arrow debug / img size adapted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 // Declare global variables used in this script
-const navLinks = ["Projets", "Contact", "Mon CV"];
+const navLinks = ["Projets", "À propos", "Contact", "Mon CV"];
+const smallArrows = document.querySelectorAll(".small-arrow");
+const arrows = document.querySelectorAll(".arrow");
+const mockUps = document.querySelectorAll(".mockUp");
 const Dates = new Date();
 const year = Dates.getFullYear();
-const mockUps = document.querySelectorAll(".mockUp");
 
 // mobile menu class adding and CTA's innerText adaptation
 document.addEventListener("click", (e) => {
@@ -28,6 +30,22 @@ document.addEventListener("click", (e) => {
   }
 });
 
+smallArrows.forEach(function darkTheme(arrow) {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    arrow.src = "./medias/arrow burger menu white.svg"
+  } else {
+    arrow.src = "./medias/arrow burger menu.svg"
+  }
+})
+arrows.forEach(function darkTheme(arrow) {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    arrow.src = "./medias/arrow projects white.svg"
+  } else {
+    arrow.src = "./medias/arrow projects.svg"
+
+  }
+});
+
 // document.addEventListener('DOMContentLoaded', () => {
 //   setTimeout(() => {
 //     loader.innerText = "Welcome !"
@@ -39,7 +57,6 @@ document.addEventListener("click", (e) => {
 // })
 
 mockUps.forEach(function hoverPlay(mockUp) {
-  mockUp.play();
   mockUp.addEventListener("mouseenter", () => {
     mockUp.play();
   });
