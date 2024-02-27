5ab8b96633c20d9af3fd69ec4e4cc96196b1d42d
LeWilSy <b.tighidet0@gmail.com>
2024-02-27 03:13:43 +0100

    layout rework / img into video / mobile first

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,31 @@
 const navLinks = ["Projets", "Contact", "Mon CV"];
 const Dates = new Date();
 const year = Dates.getFullYear();
+const mockUps = document.querySelectorAll(".mockUp");
 
 // mobile menu class adding and CTA's innerText adaptation
 document.addEventListener("click", (e) => {
   if (e.target.innerText === "Menu") {
     mobileNavClicked.classList.toggle("mobile-nav-clicked");
-    test2.classList.toggle("mobile-nav-clicked");
-    test3.classList.toggle("mobile-nav-clicked");
+    offbeat1.classList.toggle("mobile-nav-clicked");
+    offbeat2.classList.toggle("mobile-nav-clicked");
+    offbeat3.classList.toggle("mobile-nav-clicked");
     mobileNav.innerText = "X";
   } else if (e.target.innerText === "X") {
     mobileNavClicked.classList.toggle("mobile-nav-clicked");
-    test2.classList.toggle("mobile-nav-clicked");
-    test3.classList.toggle("mobile-nav-clicked");
+    offbeat1.classList.toggle("mobile-nav-clicked");
+    offbeat2.classList.toggle("mobile-nav-clicked");
+    offbeat3.classList.toggle("mobile-nav-clicked");
     mobileNav.innerText = "Menu";
   }
   if (navLinks.includes(e.target.innerText)) {
     mobileNavClicked.classList.toggle("mobile-nav-clicked");
-    test2.classList.toggle("mobile-nav-clicked");
-    test3.classList.toggle("mobile-nav-clicked");
+    offbeat1.classList.toggle("mobile-nav-clicked");
+    offbeat2.classList.toggle("mobile-nav-clicked");
+    offbeat3.classList.toggle("mobile-nav-clicked");
     mobileNav.innerText = "Menu";
   }
-})
+});
 
 // document.addEventListener('DOMContentLoaded', () => {
 //   setTimeout(() => {
@@ -34,4 +38,13 @@ document.addEventListener("click", (e) => {
 //   }, 1100)
 // })
 
-credits.innerText = "Billel Tighidet © " + year + " - Tous droits réservés"
+mockUps.forEach(function hoverPlay(mockUp) {
+  mockUp.addEventListener("mouseenter", () => {
+    mockUp.play();
+  });
+  mockUp.addEventListener("mouseleave", () => {
+    mockUp.pause();
+  });
+});
+
+credits.innerText = "© " + year + " Billel Tighidet Tous droits réservés";
