8d3d0e4191843afc576d69d2636bb2745b6a561d
LeWilSy <b.tighidet0@gmail.com>
2024-04-12 11:21:48 +0200

    fix: mousemove cursor on mobile & card container rotation for mobile with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 const navLinks = ["À propos", "Projets", "Contact", "Mon CV"];
 const headTranslateLeft = document.querySelectorAll(".from-left");
 const headTranslateRight = document.querySelectorAll(".from-right");
+const imgGrid = document.getElementById("imgGrid");
+const cardContainers = document.querySelectorAll(".card-container");
 const Dates = new Date();
 const year = Dates.getFullYear();
 const menuButton = document.getElementById("menuButton");
@@ -38,10 +40,30 @@ header.addEventListener("click", (e) => {
 });
 
 // give cursor position
-document.addEventListener("mousemove", (e) => {
-  cursor.classList.add("visible");
-  cursor.style.transform = "translate(" + e.pageX + "px," + e.pageY + "px)";
-});
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+  cardContainers.forEach(cardContainer => {
+    cardContainer.addEventListener("touchend", () => {
+      cardContainer.classList.toggle("rotate")
+    })
+  });
+} else {
+  document.addEventListener("mousemove", (e) => {
+    cursor.classList.add("visible");
+    cursor.style.transform = "translate(" + e.pageX + "px," + e.pageY + "px)";
+  });
+  imgGrid.addEventListener("mouseover", () => {
+    cardContainer.classList.add("rotate")
+  })
+  cardContainers.forEach(cardContainer => {
+    cardContainer.addEventListener("mouseenter", () => {
+      cardContainer.classList.add("rotate")
+    })
+    cardContainer.addEventListener("mouseleave", () => {
+      cardContainer.classList.remove("rotate")
+    })
+  });
+}
+
 
 document.addEventListener("mouseleave", (e) => {
   cursor.classList.remove("visible");
