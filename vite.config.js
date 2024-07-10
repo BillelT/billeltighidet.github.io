126827ac00d474cca1a60746143d2a6fe74075d9
Billel T <148473122+BillelT@users.noreply.github.com>
2024-07-10 07:06:12 +0200

    fix / live 404

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Dossier de sortie pour les fichiers construits
+    sourcemap: false, // Générer des sourcemaps si nécessaire
+    minify: true, // Minification des fichiers en production
+  },
+});
