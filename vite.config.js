690d7b51d32f833d6d045fbd6d3b09a8797bd072
LeWilSy <b.tighidet0@gmail.com>
2024-08-07 16:13:59 +0200

    fix: robots.txt && publicDir declared

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,5 @@ export default defineConfig({
     host: true, // Open to local network and display URL
     open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
   },
+  publicDir: "public",
 });
