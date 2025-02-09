5674782c4720f62beafb4de31f6563375f84da48
LeWilSy <b.tighidet0@gmail.com>
2025-02-09 17:49:11 +0100

    3d plane rezsponsive done also on project page

--- src/experience/Experience.jsx
+++ src/experience/Experience.jsx
@@ -43,18 +43,27 @@ export default function Experience({ isProjectPage }) {
 
       // Adapter la taille du Plane pour qu'il ne dépasse pas
 
-      if (isScreenLarger960) plane.scale.setScalar(width * 0.325);
+      if (!isScreenLarger960) {
+        plane.scale.setScalar(width * 0.5);
 
-      if (!isScreenLarger960) plane.scale.setScalar(width * 0.5);
+        plane.position.x = 0;
 
-      if (isScreenLarger960)
+        plane.position.y = index === 0 ? -4.15 : index === 1 ? -7.9 : -11.65;
+
+        if (isProjectPage)
+          plane.position.y = index === 0 ? -4.75 : index === 1 ? -7.75 : -10.75;
+      }
+
+      if (isScreenLarger960) {
+        plane.scale.setScalar(width * 0.325);
+
         plane.position.x =
           index % 2 === 0
             ? -width / 2 + plane.scale.x * 1
             : width / 2 - plane.scale.x * 0.95;
 
-      // Handle position depending on isScreenLarger960 state because of the media queries
-      if (!isScreenLarger960) plane.position.x = 0;
+        plane.position.y = index === 0 ? -3.75 : index === 1 ? -6.8 : -9.85;
+      }
     });
 
     renderer.current.setSize(sizes.current.width, sizes.current.height);
@@ -88,7 +97,8 @@ export default function Experience({ isProjectPage }) {
     const planeGeometry = new THREE.PlaneGeometry(1.6, 0.9);
 
     htmlElements.forEach((htmlElement, index) => {
-      const texture = textureLoader.load(`/img/mockups/${index + 1}.png`);
+      const texture = textureLoader.load(`/img/mockups/1.png`);
+      // const texture = textureLoader.load(`/img/mockups/${index + 1}.png`);
       texture.minFilter = THREE.LinearFilter;
       texture.magFilter = THREE.LinearFilter;
       texture.colorSpace = THREE.SRGBColorSpace;
@@ -98,9 +108,6 @@ export default function Experience({ isProjectPage }) {
         transparent: true,
       });
       const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-
-      if (isScreenLarger960) plane.position.y = index % 2 === 0 ? -3.75 : -6.8;
-      if (!isScreenLarger960) plane.position.y = index % 2 === 0 ? -4.15 : -7.9;
 
       projectsGroup.current.add(plane);
       planes.current[index] = { plane, htmlElement, index };
@@ -132,9 +139,9 @@ export default function Experience({ isProjectPage }) {
     gsap.to(camera.current, {
       scrollTrigger: {
         trigger: "#projects",
-        start: isProjectPage ? "-10% bottom" : "top 85%",
-        end: isProjectPage ? "200% top" : "150% top",
-        // markers: true,
+        start: isProjectPage ? "-15% bottom" : "top 85%",
+        end: isProjectPage ? "115% top" : "150% top",
+        markers: true,
         toggleActions: "play none reverse none",
         onUpdate: (self) => {
           camera.current.position.y = -self.progress * 15;
